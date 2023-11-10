1f2e11420d7d5b48e50a28dd2d152c4761889c9b
MarkussIPA22 <125544038+MarkussIPA22@users.noreply.github.com>
2023-11-10 09:52:07 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import {useState, useEffect} from "react";
 import Diena from './Diena';
 import Styles from './Janis.module.css'
 
-function App(props){
+function App(props) { 
+
+const [lessons, setLessons] = useState({});
+const [loading, setLoading] = useState(true);
+
   const PirmdienasStundas = [
       "Sports",
       "DabasZinibas",
@@ -49,18 +54,37 @@ function App(props){
         "Matematika"
       ]}
     ];
-    
+    useEffect(() => {
+async function getData() {
+
+const response = await fetch("https://cheese-cake.onthewifi.com/api/lessons")
+
+const data = await response.json()
+
+console.log(data.IPa22[4].classes)
+setLessons(data)
+setLoading(false)
+}
+getData() 
+    }, [] );
+  
     const dienasJSX = visasStundas.map((diena, indekss) => {
       return <Diena key={indekss} diena={diena.diena} stundas={diena.stundas} />
+       return(
     })
 
-  //export default function App
+  //export default function App 
+ 
+  
+  }
   return(
     <>
     <div>
   <ol>{dienasJSX}</ol>
     </div>
+    {loading ? <p>Loading...</p>: <p>iet plz</p>}
     </>
-  );
-}
+   ) ;
+
+
 export default App;
